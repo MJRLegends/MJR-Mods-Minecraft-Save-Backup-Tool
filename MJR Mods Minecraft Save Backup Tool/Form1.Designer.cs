fee9146f2ad2dc66fbc78b67bb15b3dd8d75f472
MJRLegends <MJRLegends@users.noreply.github.com>
2018-03-29 23:19:22 +0100

    Initial commit

---
+++ MJR Mods Minecraft Save Backup Tool/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace MJR_Mods_Minecraft_Save_Backup_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnBackup = new DevComponents.DotNetBar.ButtonX();
+            this.txtFolderPath = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.btnFolderExplorer = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtOutput = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.btnFindSaves = new DevComponents.DotNetBar.ButtonX();
+            this.listSaveResults = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnBackup.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnBackup.Enabled = false;
+            this.btnBackup.Location = new System.Drawing.Point(297, 289);
+            this.btnBackup.Name = "buttonX1";
+            this.btnBackup.Size = new System.Drawing.Size(279, 26);
+            this.btnBackup.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnBackup.TabIndex = 0;
+            this.btnBackup.Text = "Backup";
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.BackColor = System.Drawing.Color.White;
+            // 
+            // 
+            // 
+            this.txtFolderPath.Border.BackColor = System.Drawing.Color.White;
+            this.txtFolderPath.Border.BackColor2 = System.Drawing.Color.White;
+            this.txtFolderPath.Border.Class = "TextBoxBorder";
+            this.txtFolderPath.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtFolderPath.ForeColor = System.Drawing.Color.Black;
+            this.txtFolderPath.Location = new System.Drawing.Point(12, 12);
+            this.txtFolderPath.Name = "textBoxX1";
+            this.txtFolderPath.ReadOnly = true;
+            this.txtFolderPath.Size = new System.Drawing.Size(250, 20);
+            this.txtFolderPath.TabIndex = 1;
+            // 
+            // btnFolderExplorer
+            // 
+            this.btnFolderExplorer.Location = new System.Drawing.Point(261, 12);
+            this.btnFolderExplorer.Name = "btnFolderExplorer";
+            this.btnFolderExplorer.Size = new System.Drawing.Size(25, 20);
+            this.btnFolderExplorer.TabIndex = 2;
+            this.btnFolderExplorer.Text = "...";
+            this.btnFolderExplorer.UseVisualStyleBackColor = true;
+            this.btnFolderExplorer.Click += new System.EventHandler(this.btnFolderExplorer_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.BackColor = System.Drawing.Color.White;
+            // 
+            // 
+            // 
+            this.txtOutput.Border.BackColor = System.Drawing.Color.White;
+            this.txtOutput.Border.BackColor2 = System.Drawing.Color.White;
+            this.txtOutput.Border.Class = "TextBoxBorder";
+            this.txtOutput.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtOutput.ForeColor = System.Drawing.Color.Black;
+            this.txtOutput.Location = new System.Drawing.Point(12, 38);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtSavesResults";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(279, 277);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // btnFindSaves
+            // 
+            this.btnFindSaves.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnFindSaves.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnFindSaves.Enabled = false;
+            this.btnFindSaves.Location = new System.Drawing.Point(297, 12);
+            this.btnFindSaves.Name = "buttonX2";
+            this.btnFindSaves.Size = new System.Drawing.Size(279, 29);
+            this.btnFindSaves.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnFindSaves.TabIndex = 4;
+            this.btnFindSaves.Text = "Find Saves";
+            this.btnFindSaves.Click += new System.EventHandler(this.btnFindSaves_Click);
+            // 
+            // listSaveResults
+            // 
+            this.listSaveResults.CheckBoxes = true;
+            this.listSaveResults.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listSaveResults.Location = new System.Drawing.Point(298, 47);
+            this.listSaveResults.Name = "listSaveResults";
+            this.listSaveResults.Size = new System.Drawing.Size(278, 236);
+            this.listSaveResults.TabIndex = 5;
+            this.listSaveResults.UseCompatibleStateImageBehavior = false;
+            this.listSaveResults.View = System.Windows.Forms.View.SmallIcon;
+            this.listSaveResults.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.listView1_ItemCheck);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Save Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 327);
+            this.Controls.Add(this.listSaveResults);
+            this.Controls.Add(this.btnFindSaves);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnFolderExplorer);
+            this.Controls.Add(this.txtFolderPath);
+            this.Controls.Add(this.btnBackup);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(595, 366);
+            this.MinimumSize = new System.Drawing.Size(595, 366);
+            this.Name = "Form1";
+            this.Text = "MJR Mods - Minecraft Save Backup Tool v1.0.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.ButtonX btnBackup;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtFolderPath;
+        private System.Windows.Forms.Button btnFolderExplorer;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtOutput;
+        private DevComponents.DotNetBar.ButtonX btnFindSaves;
+        private System.Windows.Forms.ListView listSaveResults;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ MJR Mods Minecraft Save Backup Tool/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using Ionic.Zip;
+
+namespace MJR_Mods_Minecraft_Save_Backup_Tool
+{
+    public partial class Form1 : DevComponents.DotNetBar.Metro.MetroForm
+    {
+        String path = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFolderExplorer_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+            path = fbd.SelectedPath;
+            txtFolderPath.Text = path;
+            btnFindSaves.Enabled = true;
+        }
+
+        String[] filePaths;
+        private void btnBackup_Click(object sender, EventArgs e)
+        {
+            if (listSaveResults.CheckedItems.Count > 0)
+            {
+                foreach (String pathLoop in filePaths)
+                {
+                    if (!pathLoop.Contains("_mcr") && !pathLoop.Contains("_old"))
+                    {
+                        if (pathLoop.Contains("level.dat") && pathLoop.Contains("saves"))
+                        {
+                            if (!Directory.Exists(@"c:\Backups"))
+                            {
+                                Directory.CreateDirectory(@"c:\Backups");
+                            }
+                            Console.WriteLine(pathLoop);
+                            try
+                            {
+                                using (ZipFile zip = new ZipFile())
+                                {
+                                    zip.ParallelDeflateThreshold = -1;
+                                    zip.AddDirectory(pathLoop.Substring(0, pathLoop.LastIndexOf(@"\")));
+                                    String name = "";
+                                    String folderName = "";
+
+                                    if (pathLoop.Contains(@"minecraft\saves\") && pathLoop.Contains(@"\instances\"))
+                                    {
+                                        name = pathLoop.ToLower().Substring(pathLoop.IndexOf("minecraft\\saves\\") + 16);
+                                        folderName = pathLoop.ToLower().Substring(pathLoop.IndexOf(@"instances\") + 10);
+                                        folderName = folderName.Substring(0, folderName.IndexOf(@"\"));
+                                    }
+                                    else if (pathLoop.Contains(@"\saves"))
+                                    {
+                                        name = pathLoop.ToLower().Substring(pathLoop.IndexOf(@"\saves\") + 7);
+                                        if (pathLoop.Contains(@"ftblauncher\"))
+                                        {
+                                            folderName = pathLoop.ToLower().Substring(pathLoop.IndexOf(@"ftblauncher\") + 12);
+                                        }
+                                        else if (pathLoop.Contains(@"ATLauncher\Instances\"))
+                                        {
+                                            folderName = pathLoop.ToLower().Substring(pathLoop.IndexOf(@"Instances\") + 10);
+                                        }
+                                        else if (pathLoop.Contains(@"Curse\Instances\") || pathLoop.Contains(@"Twitch\Instances\"))
+                                        {
+                                            folderName = pathLoop.ToLower().Substring(pathLoop.IndexOf(@"Instances\") + 10);
+                                        }
+                                        else
+                                        {
+                                            if (!pathLoop.Contains("Curse") && !pathLoop.Contains("Twitch"))
+                                                folderName = folderName.Substring(0, folderName.IndexOf(@"\"));
+                                        }
+                                    }
+                                    else
+                                    {
+                                        txtOutput.AppendText(Environment.NewLine + "error with " + pathLoop);
+                                        return;
+                                    }
+                                    folderName = folderName.ToLower();
+                                    name = name.Substring(0, name.IndexOf('\\'));
+
+
+                                    foreach (ListViewItem item in this.listSaveResults.CheckedItems)
+                                    {
+                                        if (item.Text.ToLower().Equals(folderName + " - " + name))
+                                        {
+                                            if (!Directory.Exists(@"c:\Backups\" + folderName))
+                                            {
+                                                Directory.CreateDirectory(@"c:\Backups\" + folderName);
+                                            }
+                                            txtOutput.AppendText(Environment.NewLine + "------Saving - " + name);
+                                            zip.Save(@"c:\Backups\" + folderName + @"\" + name + " " + DateTime.Now.Year.ToString() + "." + DateTime.Now.Month.ToString() + "." + DateTime.Now.Day.ToString() + "." + DateTime.Now.Hour.ToString() + "." + DateTime.Now.Minute.ToString() + ".zip".Replace(@"\", ""));
+                                            zip.Dispose();
+                                            txtOutput.AppendText(Environment.NewLine + "------Saved - " + name);
+
+                                        }
+                                    }
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                        }
+                    }
+                }
+            }
+            else
+                MessageBox.Show("You need to select one or more saves!", "Error!");
+        }
+
+        private void btnFindSaves_Click(object sender, EventArgs e)
+        {
+            filePaths = Directory.GetFiles("" + path, "*.dat", SearchOption.AllDirectories);
+            foreach (String pathLoop in filePaths)
+            {
+                if (!pathLoop.Contains("_mcr") && !pathLoop.Contains("_old"))
+                {
+                    if (pathLoop.Contains("level.dat") && pathLoop.Contains("saves"))
+                    {
+                        String name = "";
+                        String folderName = "";
+                        if (pathLoop.Contains(@"minecraft\saves\") && pathLoop.Contains(@"\instances\"))
+                        {
+                            name = pathLoop.ToLower().Substring(pathLoop.IndexOf("minecraft\\saves\\") + 16);
+                            folderName = pathLoop.ToLower().Substring(pathLoop.IndexOf(@"instances\") + 10);
+                            folderName = folderName.Substring(0, folderName.IndexOf(@"\"));
+                        }
+                        else if (pathLoop.Contains(@"\saves"))
+                        {
+                            name = pathLoop.ToLower().Substring(pathLoop.IndexOf(@"\saves\") + 7);
+                            if (pathLoop.Contains(@"ftblauncher\"))
+                            {
+                                folderName = pathLoop.ToLower().Substring(pathLoop.IndexOf(@"ftblauncher\") + 12);
+                            }
+                            else if (pathLoop.Contains(@"ATLauncher\Instances\"))
+                            {
+                                folderName = pathLoop.ToLower().Substring(pathLoop.IndexOf(@"Instances\") + 10);
+                            }
+                            else if (pathLoop.Contains(@"Curse\Instances\") || pathLoop.Contains(@"Twitch\Instances\"))
+                            {
+                                folderName = pathLoop.ToLower().Substring(pathLoop.IndexOf(@"Instances\") + 10);
+                            }
+                            else
+                            {
+                                if(!pathLoop.Contains("Curse") && !pathLoop.Contains("Twitch"))
+                                    folderName = folderName.Substring(0, folderName.IndexOf(@"\"));
+                            }
+                        }
+                        else
+                        {
+                            txtOutput.AppendText(Environment.NewLine + "error with " + pathLoop);
+                            return;
+                        }
+                        folderName = folderName.ToLower();
+                        name = name.Substring(0, name.IndexOf('\\'));
+                        listSaveResults.Items.Add(folderName + " - " + name);
+                    }
+                }
+            }
+        }
+        private void listView1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            btnBackup.Enabled = true;
+        }
+    }
+}
